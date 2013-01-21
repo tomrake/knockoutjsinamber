4c23fcb66b001001a45c85da23d7e808c1142489
Thomas Rake <tom.rake@gmail.com>
2013-01-21 17:51:07 -0500

    TODO: Resolve loading callback issue

--- testing/js/Knockout.deploy.js
+++ testing/js/Knockout.deploy.js
@@ -26,8 +26,14 @@ $1=smalltalk.send(self,"_isLoaded",[]);
 if(smalltalk.assert($1)){
 smalltalk.send(callback,"_value",[]);
 } else {
-$.ajax({url:"http://ajax.aspnetcdn.com/ajax/knockout/knockout-2.2.1.js",datatype:"script",success:callback});;
+
+              var script = document.createElement("script");
+  script.src = "http://ajax.aspnetcdn.com/ajax/knockout/knockout-2.2.1.js";
+  script.type = "text/javascript";
+  document.getElementsByTagName("head")[0].appendChild(script);
+            ;
 ;
+smalltalk.send(callback,"_value",[]);
 };
 return self}
 }),
--- testing/js/Knockout.js
+++ testing/js/Knockout.js
@@ -32,12 +32,18 @@ $1=smalltalk.send(self,"_isLoaded",[]);
 if(smalltalk.assert($1)){
 smalltalk.send(callback,"_value",[]);
 } else {
-$.ajax({url:"http://ajax.aspnetcdn.com/ajax/knockout/knockout-2.2.1.js",datatype:"script",success:callback});;
+
+              var script = document.createElement("script");
+  script.src = "http://ajax.aspnetcdn.com/ajax/knockout/knockout-2.2.1.js";
+  script.type = "text/javascript";
+  document.getElementsByTagName("head")[0].appendChild(script);
+            ;
 ;
+smalltalk.send(callback,"_value",[]);
 };
 return self},
 args: ["callback"],
-source: "load: callback\x0a\x09\x22Load knockoutjs and do callback when loaded\x22\x0a    self isLoaded \x0a    \x09ifTrue:[callback value]\x0a\x09\x09ifFalse:[\x0a            <$.ajax({url:\x22http://ajax.aspnetcdn.com/ajax/knockout/knockout-2.2.1.js\x22,datatype:\x22script\x22,success:callback});>]",
+source: "load: callback\x0a\x09\x22Load knockoutjs and do callback when loaded\x22\x0a    self isLoaded \x0a    \x09ifTrue:[callback value]\x0a\x09\x09ifFalse:[\x0a            <\x0a              var script = document.createElement(\x22script\x22);\x0a  script.src = \x22http://ajax.aspnetcdn.com/ajax/knockout/knockout-2.2.1.js\x22;\x0a  script.type = \x22text/javascript\x22;\x0a  document.getElementsByTagName(\x22head\x22)[0].appendChild(script);\x0a            >.\x0a            \x22Should spin here  (self isLoaded) whileFalse:[self sleep]\x22\x0a            callback value\x0a]",
 messageSends: ["ifTrue:ifFalse:", "value", "isLoaded"],
 referencedClasses: []
 }),
