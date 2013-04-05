b332a08fd26ea0e880e382410bd8ac3940275b6f
Thomas Rake <tom.rake@gmail.com>
2013-04-05 15:54:04 -0400

    WIP

--- testing/js/Knockout.deploy.js
+++ testing/js/Knockout.deploy.js
@@ -2,6 +2,28 @@ smalltalk.addPackage('Knockout');
 smalltalk.addClass('Knockout', smalltalk.Object, [], 'Knockout');
 
 smalltalk.addMethod(
+"_applyBindings_",
+smalltalk.method({
+selector: "applyBindings:",
+fn: function (anObject){
+var self=this;
+return smalltalk.withContext(function($ctx1) { return ko.applyBindings(anObject);
+return self}, function($ctx1) {$ctx1.fill(self,"applyBindings:",{anObject:anObject},smalltalk.Knockout.klass)})},
+messageSends: []}),
+smalltalk.Knockout.klass);
+
+smalltalk.addMethod(
+"_computed_with_",
+smalltalk.method({
+selector: "computed:with:",
+fn: function (aBlock,anObject){
+var self=this;
+return smalltalk.withContext(function($ctx1) { return ko.computed(aBlock,anObject);
+return self}, function($ctx1) {$ctx1.fill(self,"computed:with:",{aBlock:aBlock,anObject:anObject},smalltalk.Knockout.klass)})},
+messageSends: []}),
+smalltalk.Knockout.klass);
+
+smalltalk.addMethod(
 "_isLoaded",
 smalltalk.method({
 selector: "isLoaded",
@@ -32,6 +54,17 @@ messageSends: ["ifTrue:ifFalse:", "value", "primitiveLoad:", "isLoaded"]}),
 smalltalk.Knockout.klass);
 
 smalltalk.addMethod(
+"_observable_",
+smalltalk.method({
+selector: "observable:",
+fn: function (aString){
+var self=this;
+return smalltalk.withContext(function($ctx1) { return ko.observable(aString);
+return self}, function($ctx1) {$ctx1.fill(self,"observable:",{aString:aString},smalltalk.Knockout.klass)})},
+messageSends: []}),
+smalltalk.Knockout.klass);
+
+smalltalk.addMethod(
 "_primitiveLoad_",
 smalltalk.method({
 selector: "primitiveLoad:",
@@ -50,28 +83,40 @@ smalltalk.method({
 selector: "renderOn:",
 fn: function (html){
 var self=this;
-return smalltalk.withContext(function($ctx1) { var $1,$2;
+return smalltalk.withContext(function($ctx1) { var $1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12;
 _st(_st(html)._p())._with_((function(){
-return smalltalk.withContext(function($ctx2) {"First Name: ";
-return _st(_st(html)._strong())._at_put_("data-bind","text: firstName");
+return smalltalk.withContext(function($ctx2) {$1=_st(html)._button();
+_st($1)._onClick_((function(){
+return smalltalk.withContext(function($ctx3) {return _st((smalltalk.Browser || Browser))._open();
+}, function($ctx3) {$ctx3.fillBlock({},$ctx1)})}));
+$2=_st($1)._with_("class browser");
+return $2;
 }, function($ctx2) {$ctx2.fillBlock({},$ctx1)})}));
-_st(_st(html)._p())._with_((function(){
-return smalltalk.withContext(function($ctx2) {"Last Name: ";
-return _st(_st(html)._strong())._at_put_("data-bind","text: lastName");
+$3=_st(html)._p();
+_st($3)._with_("First Name: ");
+$4=_st($3)._with_((function(){
+return smalltalk.withContext(function($ctx2) {return _st(_st(html)._strong())._at_put_("data-bind","text: firstName");
 }, function($ctx2) {$ctx2.fillBlock({},$ctx1)})}));
-_st(_st(html)._p())._with_((function(){
-return smalltalk.withContext(function($ctx2) {"First Name: ";
-return _st(_st(html)._input())._at_put_("data-bind","text: firstName");
+$5=_st(html)._p();
+_st($5)._with_("Last Name: ");
+$6=_st($5)._with_((function(){
+return smalltalk.withContext(function($ctx2) {return _st(_st(html)._strong())._at_put_("data-bind","text: lastName");
 }, function($ctx2) {$ctx2.fillBlock({},$ctx1)})}));
-_st(_st(html)._p())._with_((function(){
-return smalltalk.withContext(function($ctx2) {"Last Name: ";
-return _st(_st(html)._input())._at_put_("data-bind","text: lastName");
+$7=_st(html)._p();
+_st($7)._with_("First Name: ");
+$8=_st($7)._with_((function(){
+return smalltalk.withContext(function($ctx2) {return _st(_st(html)._input())._at_put_("data-bind","text: firstName");
 }, function($ctx2) {$ctx2.fillBlock({},$ctx1)})}));
-$1=_st(html)._button();
-_st($1)._at_put_("data-bind","click: capitalizeLastName");
-$2=_st($1)._with_("Go Caps");
+$9=_st(html)._p();
+_st($9)._with_("Last Name: ");
+$10=_st($9)._with_((function(){
+return smalltalk.withContext(function($ctx2) {return _st(_st(html)._input())._at_put_("data-bind","text: lastName");
+}, function($ctx2) {$ctx2.fillBlock({},$ctx1)})}));
+$11=_st(html)._button();
+_st($11)._at_put_("data-bind","click: capitalizeLastName");
+$12=_st($11)._with_("Go Caps");
 return self}, function($ctx1) {$ctx1.fill(self,"renderOn:",{html:html},smalltalk.KnockoutExample)})},
-messageSends: ["with:", "at:put:", "strong", "p", "input", "button"]}),
+messageSends: ["with:", "onClick:", "open", "button", "p", "at:put:", "strong", "input"]}),
 smalltalk.KnockoutExample);
 
 
@@ -81,9 +126,11 @@ smalltalk.method({
 selector: "begin",
 fn: function (){
 var self=this;
-smalltalk.send(smalltalk.send(self,"_new",[]),"_appendToJQuery_",[smalltalk.send("body","_asJQuery",[])]);
-return self},
-messageSends: ["appendToJQuery:", "asJQuery", "new"]}),
+return smalltalk.withContext(function($ctx1) { _st((smalltalk.Knockout || Knockout))._load_((function(){
+return smalltalk.withContext(function($ctx2) {return _st(_st(self)._new())._appendToJQuery_(_st("body")._asJQuery());
+}, function($ctx2) {$ctx2.fillBlock({},$ctx1)})}));
+return self}, function($ctx1) {$ctx1.fill(self,"begin",{},smalltalk.KnockoutExample.klass)})},
+messageSends: ["load:", "appendToJQuery:", "asJQuery", "new"]}),
 smalltalk.KnockoutExample.klass);
 
 
--- testing/js/Knockout.js
+++ testing/js/Knockout.js
@@ -2,6 +2,38 @@ smalltalk.addPackage('Knockout');
 smalltalk.addClass('Knockout', smalltalk.Object, [], 'Knockout');
 
 smalltalk.addMethod(
+"_applyBindings_",
+smalltalk.method({
+selector: "applyBindings:",
+category: 'not yet classified',
+fn: function (anObject){
+var self=this;
+return smalltalk.withContext(function($ctx1) { return ko.applyBindings(anObject);
+return self}, function($ctx1) {$ctx1.fill(self,"applyBindings:",{anObject:anObject},smalltalk.Knockout.klass)})},
+args: ["anObject"],
+source: "applyBindings: anObject\x0a\x09<return ko.applyBindings(anObject)>",
+messageSends: [],
+referencedClasses: []
+}),
+smalltalk.Knockout.klass);
+
+smalltalk.addMethod(
+"_computed_with_",
+smalltalk.method({
+selector: "computed:with:",
+category: 'not yet classified',
+fn: function (aBlock,anObject){
+var self=this;
+return smalltalk.withContext(function($ctx1) { return ko.computed(aBlock,anObject);
+return self}, function($ctx1) {$ctx1.fill(self,"computed:with:",{aBlock:aBlock,anObject:anObject},smalltalk.Knockout.klass)})},
+args: ["aBlock", "anObject"],
+source: "computed: aBlock with: anObject\x0a\x09<return ko.computed(aBlock,anObject)>",
+messageSends: [],
+referencedClasses: []
+}),
+smalltalk.Knockout.klass);
+
+smalltalk.addMethod(
 "_isLoaded",
 smalltalk.method({
 selector: "isLoaded",
@@ -42,6 +74,22 @@ referencedClasses: []
 smalltalk.Knockout.klass);
 
 smalltalk.addMethod(
+"_observable_",
+smalltalk.method({
+selector: "observable:",
+category: 'not yet classified',
+fn: function (aString){
+var self=this;
+return smalltalk.withContext(function($ctx1) { return ko.observable(aString);
+return self}, function($ctx1) {$ctx1.fill(self,"observable:",{aString:aString},smalltalk.Knockout.klass)})},
+args: ["aString"],
+source: "observable: aString\x0a\x09<return ko.observable(aString)>",
+messageSends: [],
+referencedClasses: []
+}),
+smalltalk.Knockout.klass);
+
+smalltalk.addMethod(
 "_primitiveLoad_",
 smalltalk.method({
 selector: "primitiveLoad:",
@@ -66,31 +114,43 @@ selector: "renderOn:",
 category: 'not yet classified',
 fn: function (html){
 var self=this;
-return smalltalk.withContext(function($ctx1) { var $1,$2;
+return smalltalk.withContext(function($ctx1) { var $1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12;
 _st(_st(html)._p())._with_((function(){
-return smalltalk.withContext(function($ctx2) {"First Name: ";
-return _st(_st(html)._strong())._at_put_("data-bind","text: firstName");
+return smalltalk.withContext(function($ctx2) {$1=_st(html)._button();
+_st($1)._onClick_((function(){
+return smalltalk.withContext(function($ctx3) {return _st((smalltalk.Browser || Browser))._open();
+}, function($ctx3) {$ctx3.fillBlock({},$ctx1)})}));
+$2=_st($1)._with_("class browser");
+return $2;
 }, function($ctx2) {$ctx2.fillBlock({},$ctx1)})}));
-_st(_st(html)._p())._with_((function(){
-return smalltalk.withContext(function($ctx2) {"Last Name: ";
-return _st(_st(html)._strong())._at_put_("data-bind","text: lastName");
+$3=_st(html)._p();
+_st($3)._with_("First Name: ");
+$4=_st($3)._with_((function(){
+return smalltalk.withContext(function($ctx2) {return _st(_st(html)._strong())._at_put_("data-bind","text: firstName");
 }, function($ctx2) {$ctx2.fillBlock({},$ctx1)})}));
-_st(_st(html)._p())._with_((function(){
-return smalltalk.withContext(function($ctx2) {"First Name: ";
-return _st(_st(html)._input())._at_put_("data-bind","text: firstName");
+$5=_st(html)._p();
+_st($5)._with_("Last Name: ");
+$6=_st($5)._with_((function(){
+return smalltalk.withContext(function($ctx2) {return _st(_st(html)._strong())._at_put_("data-bind","text: lastName");
 }, function($ctx2) {$ctx2.fillBlock({},$ctx1)})}));
-_st(_st(html)._p())._with_((function(){
-return smalltalk.withContext(function($ctx2) {"Last Name: ";
-return _st(_st(html)._input())._at_put_("data-bind","text: lastName");
+$7=_st(html)._p();
+_st($7)._with_("First Name: ");
+$8=_st($7)._with_((function(){
+return smalltalk.withContext(function($ctx2) {return _st(_st(html)._input())._at_put_("data-bind","text: firstName");
 }, function($ctx2) {$ctx2.fillBlock({},$ctx1)})}));
-$1=_st(html)._button();
-_st($1)._at_put_("data-bind","click: capitalizeLastName");
-$2=_st($1)._with_("Go Caps");
+$9=_st(html)._p();
+_st($9)._with_("Last Name: ");
+$10=_st($9)._with_((function(){
+return smalltalk.withContext(function($ctx2) {return _st(_st(html)._input())._at_put_("data-bind","text: lastName");
+}, function($ctx2) {$ctx2.fillBlock({},$ctx1)})}));
+$11=_st(html)._button();
+_st($11)._at_put_("data-bind","click: capitalizeLastName");
+$12=_st($11)._with_("Go Caps");
 return self}, function($ctx1) {$ctx1.fill(self,"renderOn:",{html:html},smalltalk.KnockoutExample)})},
 args: ["html"],
-source: "renderOn: html\x0a\x09html p\x0a    \x09with: ['First Name: '.\x0a        \x09     html strong\x0a                 \x09at: 'data-bind' put: 'text: firstName'].\x0a    html p\x0a    \x09with: ['Last Name: '.\x0a        \x09     html strong\x0a                 \x09at: 'data-bind' put: 'text: lastName'].\x0a\x09html p\x0a    \x09with: ['First Name: '.\x0a        \x09     html input\x0a                 \x09at: 'data-bind' put: 'text: firstName'].\x0a    html p\x0a    \x09with: ['Last Name: '.\x0a        \x09     html input\x0a                 \x09at: 'data-bind' put: 'text: lastName'].\x0a    html button\x0a    \x09at: 'data-bind' put: 'click: capitalizeLastName';\x0a    \x09with:  'Go Caps'\x0a\x0a        \x09   ",
-messageSends: ["with:", "at:put:", "strong", "p", "input", "button"],
-referencedClasses: []
+source: "renderOn: html\x0a    html p\x0a\x09\x09with:[\x0a\x09\x09\x09html button onClick:[Browser open];\x0a\x09\x09\x09with:'class browser'].\x0a\x09html p\x0a    \x09with: 'First Name: ';\x0a\x09\x09with:[\x0a        \x09     html strong\x0a                 \x09at: 'data-bind' put: 'text: firstName'].\x0a    html p\x0a    \x09with: 'Last Name: ';\x0a        with: [\x09html strong\x0a                 \x09at: 'data-bind' put: 'text: lastName'].\x0a\x09html p\x0a    \x09with: 'First Name: ';\x0a\x09\x09with: [\x0a        \x09     html input\x0a                 \x09at: 'data-bind' put: 'text: firstName'].\x0a    html p\x0a    \x09with: 'Last Name: ';\x0a\x09\x09with: [\x0a        \x09     html input\x0a                 \x09at: 'data-bind' put: 'text: lastName'].\x0a    html button\x0a    \x09at: 'data-bind' put: 'click: capitalizeLastName';\x0a    \x09with:  'Go Caps'\x0a\x0a        \x09   ",
+messageSends: ["with:", "onClick:", "open", "button", "p", "at:put:", "strong", "input"],
+referencedClasses: ["Browser"]
 }),
 smalltalk.KnockoutExample);
 
@@ -102,12 +162,14 @@ selector: "begin",
 category: 'not yet classified',
 fn: function (){
 var self=this;
-smalltalk.send(smalltalk.send(self,"_new",[]),"_appendToJQuery_",[smalltalk.send("body","_asJQuery",[])]);
-return self},
+return smalltalk.withContext(function($ctx1) { _st((smalltalk.Knockout || Knockout))._load_((function(){
+return smalltalk.withContext(function($ctx2) {return _st(_st(self)._new())._appendToJQuery_(_st("body")._asJQuery());
+}, function($ctx2) {$ctx2.fillBlock({},$ctx1)})}));
+return self}, function($ctx1) {$ctx1.fill(self,"begin",{},smalltalk.KnockoutExample.klass)})},
 args: [],
-source: "begin\x0a\x09self new appendToJQuery: 'body' asJQuery",
-messageSends: ["appendToJQuery:", "asJQuery", "new"],
-referencedClasses: []
+source: "begin\x0a\x09Knockout load: [\x0a\x09\x09self new appendToJQuery: 'body' asJQuery]",
+messageSends: ["load:", "appendToJQuery:", "asJQuery", "new"],
+referencedClasses: ["Knockout"]
 }),
 smalltalk.KnockoutExample.klass);
 
