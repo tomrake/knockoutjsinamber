264b8a9aa7164586851da705cc40a2721b7dd599
Thomas Rake <tom.rake@gmail.com>
2013-12-03 13:48:21 -0500

    require[] should not have .js suffix

--- testing/js/Knockout.js
+++ testing/js/Knockout.js
@@ -1,108 +1,38 @@
-smalltalk.addPackage('Knockout', {});
+define("com_github_tomrake_knockoutjs/Knockout", ["amber_vm/smalltalk", "amber_vm/nil", "amber_vm/_st", "amber_core/Kernel-Objects"], function(smalltalk,nil,_st){
+smalltalk.addPackage('Knockout');
+smalltalk.packages["Knockout"].transport = {"type":"amd","amdNamespace":"com_github_tomrake_knockoutjs"};
+
 smalltalk.addClass('Knockout', smalltalk.Object, [], 'Knockout');
 
 smalltalk.addMethod(
-"_isLoaded",
 smalltalk.method({
 selector: "isLoaded",
 category: 'not yet classified',
-fn: function () {
-    var self = this;
-    var $1;
-    $1 = typeof ko !== "undefined";
-    return $1;
-},
+fn: function (){
+var self=this;
+return smalltalk.withContext(function($ctx1) { 
+return typeof(ko) !== 'undefined';
+return self}, function($ctx1) {$ctx1.fill(self,"isLoaded",{},smalltalk.Knockout.klass)})},
 args: [],
-source: "isLoaded\x0a\x22Test that the google.load() function is defined\x22\x0a^<typeof(ko) !== 'undefined'>",
+source: "isLoaded\x0a\x22Test that the google.load() function is defined\x22\x0a<return typeof(ko) !== 'undefined'>",
 messageSends: [],
 referencedClasses: []
 }),
 smalltalk.Knockout.klass);
 
 smalltalk.addMethod(
-"_load_",
 smalltalk.method({
 selector: "load:",
 category: 'not yet classified',
 fn: function (callback){
 var self=this;
-var $1;
-$1=smalltalk.send(self,"_isLoaded",[]);
-if(smalltalk.assert($1)){
-smalltalk.send(callback,"_value",[]);
-} else {
-<<<<<<< HEAD
-$.getScript('//cdnjs.cloudflare.com/ajax/libs/knockout/2.2.1/knockout-min.js',callback);;
-=======
-
-              var script = document.createElement("script");
-  script.src = "http://ajax.aspnetcdn.com/ajax/knockout/knockout-2.2.1.js";
-  script.type = "text/javascript";
-  document.getElementsByTagName("head")[0].appendChild(script);
-            ;
->>>>>>> 4c23fcb66b001001a45c85da23d7e808c1142489
-;
-smalltalk.send(callback,"_value",[]);
-};
-return self},
+return smalltalk.withContext(function($ctx1) { 
+return self}, function($ctx1) {$ctx1.fill(self,"load:",{callback:callback},smalltalk.Knockout.klass)})},
 args: ["callback"],
-<<<<<<< HEAD
-source: "load: callback\x0a\x09\x22Load knockoutjs and do callback when loaded\x22\x0a    self isLoaded \x0a    \x09ifTrue: [callback value]\x0a\x09\x09ifFalse: [<$.getScript('//cdnjs.cloudflare.com/ajax/libs/knockout/2.2.1/knockout-min.js',callback);>]       ",
-=======
-source: "load: callback\x0a\x09\x22Load knockoutjs and do callback when loaded\x22\x0a    self isLoaded \x0a    \x09ifTrue:[callback value]\x0a\x09\x09ifFalse:[\x0a            <\x0a              var script = document.createElement(\x22script\x22);\x0a  script.src = \x22http://ajax.aspnetcdn.com/ajax/knockout/knockout-2.2.1.js\x22;\x0a  script.type = \x22text/javascript\x22;\x0a  document.getElementsByTagName(\x22head\x22)[0].appendChild(script);\x0a            >.\x0a            \x22Should spin here  (self isLoaded) whileFalse:[self sleep]\x22\x0a            callback value\x0a]",
->>>>>>> 4c23fcb66b001001a45c85da23d7e808c1142489
-messageSends: ["ifTrue:ifFalse:", "value", "isLoaded"],
+source: "load: callback\x0a\x09",
+messageSends: [],
 referencedClasses: []
 }),
 smalltalk.Knockout.klass);
 
-
-smalltalk.addClass('ScriptLoader', smalltalk.Object, [], 'Knockout');
-
-smalltalk.addMethod(
-"_jQLoadScriptUrl_callback_",
-smalltalk.method({
-selector: "jQLoadScriptUrl:callback:",
-category: 'not yet classified',
-fn: function (url,callback){
-var self=this;
-$.getScript(url,callback);;
-;
-return self},
-args: ["url", "callback"],
-source: "jQLoadScriptUrl: url callback: callback\x0a\x09\x22comment stating purpose of message\x22\x0a<$.getScript(url,callback);>",
-messageSends: [],
-referencedClasses: []
-}),
-smalltalk.ScriptLoader.klass);
-
-smalltalk.addMethod(
-"_loadScriptUrl_callback_",
-smalltalk.method({
-selector: "loadScriptUrl:callback:",
-category: 'not yet classified',
-fn: function (url, callback) {
-    var self = this;
-    var $1, $2;
-    var script;
-    script = smalltalk.send(document, "_createElement_", ["script"]);
-    smalltalk.send(script, "_type_", ["text/javascript"]);
-    $1 = smalltalk.send(smalltalk.send(script, "_hasOwnProperty_", ["readyState"]), "_and_", [function () {return smalltalk.send(smalltalk.send(smalltalk.send(script, "_readyState", []), "__eq", [nil]), "_not", []);}]);
-    if (smalltalk.assert($1)) {
-        smalltalk.send(script, "_onreadystatechange_", [function () {$2 = smalltalk.send(smalltalk.send(smalltalk.send(script, "_readyState", []), "__eq", ["loaded"]), "_or_", [function () {return smalltalk.send(smalltalk.send(script, "_readyState", []), "__eq", ["complete"]);}]);if (smalltalk.assert($2)) {smalltalk.send(smalltalk.send(script, "_onreadystatechange", []), "__eq", [nil]);return smalltalk.send(callback, "_value", []);}}]);
-    } else {
-        smalltalk.send(script, "_inspect", []);
-        smalltalk.send(script, "_onload_", [function () {return smalltalk.send(callback, "_value", []);}]);
-    }
-    smalltalk.send(script, "_src_", [url]);
-    smalltalk.send(smalltalk.send(document, "_body", []), "_appendChild_", [script]);
-    return self;
-},
-args: ["url", "callback"],
-source: "loadScriptUrl: url callback: callback\x0a\x09\x22comment stating purpose of message\x22\x0a|script|\x0a\x09script := document createElement: 'script'.\x0a\x09script type: 'text/javascript'.\x0a\x09((script hasOwnProperty: 'readyState') and:[(script readyState = nil) not]) \x0a\x09\x09ifTrue:[\x0a\x09\x09\x09script onreadystatechange: [\x0a\x09\x09\x09\x09(((script readyState) = 'loaded') or:[(script readyState = 'complete')]) ifTrue:[\x0a\x09\x09\x09\x09\x09script onreadystatechange = nil.\x0a\x09\x09\x09\x09\x09callback value.\x0a\x09\x09\x09]]]\x0a\x09\x09 \x0a\x09 ifFalse:[\x0a        script inspect.\x0a\x09\x09script onload:[callback value]].\x0a\x09script src:url.\x0a\x09document body appendChild:script",
-messageSends: ["createElement:", "type:", "ifTrue:ifFalse:", "onreadystatechange:", "ifTrue:", "=", "onreadystatechange", "value", "or:", "readyState", "inspect", "onload:", "and:", "not", "hasOwnProperty:", "src:", "appendChild:", "body"],
-referencedClasses: []
-}),
-smalltalk.ScriptLoader.klass);
-
-
+});
